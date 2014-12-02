14ccd9cf9215d25047051b6f40200dd1cb89f927
jk222rp <jk222rp@student.lnu.se>
2014-12-02 21:35:18 +0000

    Laboration 3 - steg 8 och 9 i mojligt upplagg

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -21,29 +21,43 @@ var Memory = {
         var table = document.createElement("table");
         var count = 0;
         
+        // creates and adds each row
         for (var i = 0; i < rows; i++) {
             var tr = document.createElement("tr");
             
+            // creates and adds each column
             for (var j = 0; j < cols; j++) {
                 var td = document.createElement("td");
                 td.id = "col" + count;
                 
+                // creates the image with the back side of the "card"
                 var img = document.createElement("img");
-                img.src = "pics/" + Memory.randoms[count] + ".png";
-                //img.src = "pics/0.png";
-                td.appendChild(img);
+                //img.src = "pics/" + Memory.randoms[count] + ".png";
+                img.src = "pics/0.png";
+                
+                // creates a link around the image
+                var a = document.createElement("a");
+                a.setAttribute("href", "#");
+                a.appendChild(img);
+                a.onclick = openBrick(count, img);
+                
+                td.appendChild(a);
                 
                 tr.appendChild(td);
+                
                 ++count;
             }
             
             table.appendChild(tr);
         }
+                
+        function openBrick(count, img) {
+            return function() {
+                img.src = "pics/" + Memory.randoms[count] + ".png";
+            };
+        }
         
         container.appendChild(table);
-        
-        console.log(col1.innerHTML);
-        console.log(col2.innerHTML);
     }
     
 };
