fae21b4ccbe9e52468dc75e323681992b2cc9a98
jk222rp <jk222rp@student.lnu.se>
2014-12-03 20:53:47 +0000

    Laboration 3 - steg 10, 11 och 12 i mojligt upplagg. Klar!

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -3,63 +3,129 @@
 var Memory = {
     
     init: function() {
-        var rows = 4;
+        var rows = 3;
         var cols = 4;
         
         Memory.randoms = RandomGenerator.getPictureArray(rows, cols);
         
         Memory.createGame(rows, cols);
+        
+        Memory.playGame(rows, cols);
     },
     
-    //Array with the random numbers
+//Array with the random numbers
     randoms: [],
     
-    //Creates a table with the images in the game
+//Creates a table with the images in the game
     createGame: function(rows, cols) {
         
-        var container = document.getElementById("container");
+        var cardNr = 0;
+        var content = document.getElementById("content");
         var table = document.createElement("table");
-        var count = 0;
         
-        // creates and adds each row
+    // creates and adds each row
         for (var i = 0; i < rows; i++) {
             var tr = document.createElement("tr");
             
-            // creates and adds each column
+        // creates and adds each column
             for (var j = 0; j < cols; j++) {
                 var td = document.createElement("td");
-                td.id = "col" + count;
                 
-                // creates the image with the back side of the "card"
+            // creates the image with the back side of the card
                 var img = document.createElement("img");
-                //img.src = "pics/" + Memory.randoms[count] + ".png";
+                img.setAttribute("id", "card" + cardNr);
                 img.src = "pics/0.png";
                 
-                // creates a link around the image
+            // creates a link around the image
                 var a = document.createElement("a");
                 a.setAttribute("href", "#");
                 a.appendChild(img);
-                a.onclick = openBrick(count, img);
                 
                 td.appendChild(a);
                 
                 tr.appendChild(td);
                 
-                ++count;
+                ++cardNr;
             }
             
             table.appendChild(tr);
         }
-                
-        function openBrick(count, img) {
+        
+        content.appendChild(table);
+    },
+    
+//Makes the game playable and returns the number of turns it took to complete the game
+    playGame: function(rows, cols) {
+        var aTags = document.getElementsByTagName("a");
+        var cardNr = 0;
+        var cards = [];
+        var countTurnedCards = 0;
+        var gameEnded = false;
+        var nrOfTurnedPairs = 0;
+        var nrOfTurns = 0;
+    
+    // sets onclick-event to the a-tags around the images
+        for (var i = 0; i < aTags.length; i++) {
+            var img = aTags[i].getElementsByTagName("img")[0];
+            aTags[i].onclick = turnCard(cardNr, img);
+            
+            ++cardNr;
+        }
+        
+    // turns a card, only if 2 cards aren't already turned
+        function turnCard(cardNr, img) {
             return function() {
-                img.src = "pics/" + Memory.randoms[count] + ".png";
+            // checks that 2 cards are not turned and that the card aren't already turned
+                if (countTurnedCards < 1 && img.src[img.src.length - 5] === "0") {
+                    img.src = "pics/" + Memory.randoms[cardNr] + ".png";
+                    cards[0] = document.getElementById("card" + cardNr);
+                    ++countTurnedCards;
+                } else if (countTurnedCards < 2 && img.src[img.src.length - 5] === "0") {
+                    img.src = "pics/" + Memory.randoms[cardNr] + ".png";
+                    cards[1] = document.getElementById("card" + cardNr);
+                    ++countTurnedCards;
+                }
+                
+            // checks if 2 cards are turned and in that case turn them back after 1 second
+                if (countTurnedCards == 2 && cards[0].src !== cards[1].src) {
+                    setTimeout(function() {
+                    // turn the cards back
+                        cards[0].src = "pics/0.png";
+                        cards[1].src = "pics/0.png";
+                        
+                    // the turned cards are now 0
+                        countTurnedCards = 0;
+                    }, 1000);
+                    
+                    
+                // increases the number of turns
+                    ++nrOfTurns;
+                } else if (countTurnedCards == 2 && cards[0].src === cards[1].src) {
+                    ++nrOfTurns;
+                    ++nrOfTurnedPairs;
+                        
+                // the unmatching turned cards are now 0
+                    countTurnedCards = 0;
+                }
+                
+            // checks if all cards are turned
+                if (!gameEnded && nrOfTurnedPairs === (rows * cols)/2) {
+                    endOfGame();
+                    gameEnded = true;
+                }
+                
+            // the page won't reload
+                return false;
             };
         }
         
-        container.appendChild(table);
+        function endOfGame() {
+            var container = document.getElementById("container");
+            var p = document.createElement("p");
+            p.innerHTML = "Grattis du klarade spelet på " + nrOfTurns + " försök!";
+            container.appendChild(p);
+        }
     }
-    
 };
 
 window.onload = Memory.init;
