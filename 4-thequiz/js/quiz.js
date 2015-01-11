cb2418a51651b45a399c58c8fc1f6d012ce49a78
jk222rp <jk222rp@student.lnu.se>
2015-01-11 22:05:36 +0000

    Labb 4 - publicera fragan

--- 4-thequiz/js/quiz.js
+++ 4-thequiz/js/quiz.js
@@ -3,11 +3,11 @@
 var Quiz = {
     
     init: function() {
-        Quiz.question();
+        Quiz.startQuiz();
     },
 
 // Hämtar information om en fråga och returnerar detta som ett objekt
-    question: function() {
+    startQuiz: function() {
         var question = {};
         var xhr = new XMLHttpRequest();
         
@@ -18,26 +18,49 @@ var Quiz = {
                 
             // Publicerar frågan på webbsidan
                 Quiz.publishQuestion(question);
-                
-            // Hämtar svaret från användaren
-                Quiz.answer();
             }
         };
         
         xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
         
         xhr.send(null);
-        
-        //return question;
     },
     
 // Publicerar frågan på webbsidan
     publishQuestion: function(question) {
+        var content = document.getElementById("content");
+        var header = document.createElement("h2");
+        var p = document.createElement("p");
+        var label = document.createElement("label");
+        var input = document.createElement("input");
+        var button = document.createElement("button");
         
-    },
-    
-// Hämtar svaret från användaren
-    answer: function() {
+    // Anger vilket nummer det är på frågan
+        header.innerHTML = "Fråga " + question.id;
+        content.appendChild(header);
+        
+    // Skriver ut frågan
+        p.innerHTML = question.question;
+        content.appendChild(p);
+        
+    // Labeln för svaret
+        label.for = "answer";
+        label.innerHTML = "Svar: ";
+        content.appendChild(label);
+        
+    // Textarean för svaret
+        input.type = "text";
+        input.id = "answer";
+        input.name = "answer";
+        content.appendChild(input);
+        
+    // Knappen för att skicka svaret
+        button.innerHTML = "OK";
+        button.onclick = function() {
+        // Skickar svaret från användaren till API:et
+            Quiz.sendAnswer(input.value);
+        };
+        content.appendChild(button);
         
     },
     
