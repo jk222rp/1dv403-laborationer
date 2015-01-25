9c1da3456c29b8b562878497b82a151eeb4c6c16
jk222rp <jk222rp@student.lnu.se>
2015-01-25 18:25:13 +0000

    Projektuppgift - Tumnagelbilderna

--- 5-pwd/script/desktop.js
+++ 5-pwd/script/desktop.js
@@ -11,6 +11,7 @@ var Desktop = {
         
         openImageViewer.onclick = function() {
             Desktop.openWindow();
+            return false;
         };
     },
     
@@ -47,6 +48,7 @@ var Desktop = {
         closeButton.setAttribute("title", "Stäng Image Viewer");
         closeButton.onclick = function() {
             container.removeChild(win);
+            return false;
         };
         winHead.appendChild(closeButton);
         
@@ -65,6 +67,83 @@ var Desktop = {
     //Skapar ett fönster och lägger in i containern
         win.setAttribute("id", "window");
         container.appendChild(win);
+        
+    //Hämtar bilderna
+        Desktop.getPictures();
+    },
+    
+//Hämtar bilderna
+    getPictures: function() {
+        var pictures = [];
+        var xhr = new XMLHttpRequest();
+        
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                pictures = JSON.parse(xhr.responseText);
+                
+                Desktop.addPictures(pictures);
+            }
+        };
+        
+        xhr.open("GET", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
+        
+        xhr.send(null);
+    },
+    
+//Lägger till bilderna
+    addPictures: function(pictures) {
+        var size = [];
+        var winContent = document.getElementById("windowContent");
+        
+    //Tar fram högsta höjd och bredd av tumnagelbilderna
+        size = Desktop.getSize(pictures);
+    
+        for (var i = 0; i < pictures.length; i++) {
+            var thumbBox = document.createElement("div");
+            var thumbImg = document.createElement("img");
+            var thumbLink = document.createElement("a");
+            
+            thumbImg.src = pictures[i].thumbURL;
+            thumbImg.setAttribute("class", "images");
+            thumbBox.appendChild(thumbImg);
+        
+        //Sätter bredd och höjd på tumnagelboxarna till de högsta värdena bland de laddade bilderna
+            thumbBox.style.width = size[0].toString() + "px";
+            thumbBox.style.height = size[1].toString() + "px";
+            
+            thumbBox.setAttribute("class", "thumbBox");
+            thumbLink.appendChild(thumbBox);
+            
+            thumbLink.setAttribute("href", "#");
+            winContent.appendChild(thumbLink);
+        }
+    },
+    
+//Tar fram högsta höjd och bredd av tumnagelbilderna
+    getSize: function(pictures) {
+        var width = 0;
+        var height = 0;
+        var size = [];
+        
+    //Går igenom varje bild
+        for (var i = 0; i < pictures.length; i++) {
+        //Om bildens bredd är större än tidigare lagrat värde på bredd
+        //ändras det lagrade värdet till bildens bredd
+            if (pictures[i].thumbWidth > width) {
+                width = pictures[i].thumbWidth;
+            }
+            
+        //Om bildens höjd är större än tidigare lagrat värde på höjd
+        //ändras det lagrade värdet till bildens höjd
+            if (pictures[i].thumbHeight > height) {
+                height = pictures[i].thumbHeight;
+            }
+        }
+        
+        size[0] = width;
+        size[1] = height;
+        
+        return size;
     }
 };
 
